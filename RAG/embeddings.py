268b1b9e0ac2093fff5d60137c74655bb65448c6
mdzaid382 <mdzaid382@gmail.com>
2026-07-28 17:03:03 +0530

    added logging to embedding.py

--- RAG/embeddings.py
+++ RAG/embeddings.py
@@ -1,5 +1,7 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+from Logger import logging
+
 
 class EmbeddingModel:
     """
@@ -8,13 +10,28 @@ class EmbeddingModel:
 
     def __init__(
         self,
-        model_name: str = "models/gemini-embedding-001"
+        model_name: str = "models/gemini-embedding-001",
     ):
-        self.model_name = model_name
+        try:
+            logging.info(
+                f"Initializing embedding model: {model_name}"
+            )
 
-        self.embedding_model = GoogleGenerativeAIEmbeddings(
-            model=self.model_name
-        )
+            self.model_name = model_name
+
+            self.embedding_model = GoogleGenerativeAIEmbeddings(
+                model=self.model_name
+            )
+
+            logging.info(
+                "Embedding model initialized successfully."
+            )
+
+        except Exception:
+            logging.exception(
+                "Failed to initialize embedding model."
+            )
+            raise
 
     def get_embeddings(self):
         """
